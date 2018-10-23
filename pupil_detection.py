51abf6a72a638f04f5cb2b4a8ffb90e86e1e98b7
Asif <shaikhasif15752@gmail.com>
2018-10-23 17:42:52 +0530

    removed video starting index

--- pupil_detection.py
+++ pupil_detection.py
@@ -47,7 +47,7 @@ if not args.get("video", False):
 # otherwise, grab a reference to the video file
 else:
     vs = cv2.VideoCapture(args["video"])
-    vs.set(cv2.CAP_PROP_POS_MSEC, 33000)
+
 
 PADDING_X = 5
 PADDING_Y = 3
